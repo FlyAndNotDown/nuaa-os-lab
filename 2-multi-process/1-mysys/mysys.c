8ab62b7d86d9e9975323cb90c9224f34de1e06b8
John Kindem <461425614@qq.com>
2018-06-15 18:04:11 +0800

    完成mysys

--- 2-multi-process/1-mysys/mysys.c
+++ 2-multi-process/1-mysys/mysys.c
@@ -4,17 +4,22 @@
 #include <sys/wait.h>
 
 int mysys(char *arg) {
+    // fork一个子进程
     pid_t fpid = fork();
     if (fpid < 0) {
+        // 如果获取子进程失败
         return -1;
     } else if (fpid == 0) {
+        // 如果是子进程
         if (execl("/bin/sh", "sh", "-c", arg, (char *) 0) < 0) {
             return 127;
         }
     } else {
+        // 等待子进程结束
         wait(NULL);
         return 0;
     }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -22,6 +27,7 @@ int main(int argc, char *argv[]) {
     char buffer[200];
     buffer[0] = '\0';
     int count = 0;
+    // 将参数拼成一个字符串传递给mysys函数
     for (i = 1; i < argc; i++) {
         count += strlen(argv[i]);
         if (count < 200) {
@@ -31,6 +37,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // 调用mysys
     mysys(buffer);
 
     return 0;
